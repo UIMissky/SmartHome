268c8d6abee86a798b236df73de56df1add503a2
missky <370155330@qq.com>
2018-02-13 17:55:10 +0800

    First Creat

---
+++ SmartHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartHome
+//
+//  Created by playable-iMac2 on 2018/2/13.
+//  Copyright © 2018年 majin.missky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartHome
+//
+//  Created by playable-iMac2 on 2018/2/13.
+//  Copyright © 2018年 majin.missky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
